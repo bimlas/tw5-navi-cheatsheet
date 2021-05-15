0790adcd3c4b5bdb4ad40f92ec21cf240a341688
bimlas <bimbalaszlo@gmail.com>
2021-05-15 17:55:29 +0200

    Extracting of Navi codeblocks basically works

--- plugins/navi-cheatsheet/startup.js
+++ plugins/navi-cheatsheet/startup.js
@@ -19,10 +19,32 @@ Extract Navi cheatsheets to proper format
     exports.false = true;
 
     exports.startup = function() {
-        console.log('LOADED');
         $tw.wiki.addEventListener("change", function (changedTiddlers) {
-            console.log('Changed tiddlers are:', changedTiddlers);
+            var hasModifiedCheatsheet = Object.keys(changedTiddlers)
+                .reduce(function(results, tiddler) { return results || (!changedTiddlers[tiddler].deleted && tiddlerContainsCheatsheet(tiddler)) }, false);
+
+            if (!hasModifiedCheatsheet) {
+                return;
+            }
+
+            console.info("navi-cheatsheet: Generating cheatsheet file");
+
+            var cheatsheet = extractCheatsheet("New Tiddler");
+
+            console.log(JSON.stringify(cheatsheet));
         });
     };
+
+    function tiddlerContainsCheatsheet(tiddler) {
+        return $tw.wiki.getTiddler(tiddler).fields.text.search("@@navi") >= 0;
+    }
+
+    function extractCheatsheet(tiddler) {
+        return $tw.wiki.parseTextReference(tiddler, "text", undefined, {}).tree
+            .filter(function(block) { return block.type === "codeblock" })
+            .filter(function(codeblock) { return $tw.utils.hop(codeblock.attributes, "style") && codeblock.attributes.style.value.startsWith("navi:") })
+            .map(function(current) { return "% " + current.attributes.style.value.replace(/navi:\s*(.*);/, "$1") + "\n\n" + current.attributes.code.value })
+            .join("\n\n");
+    }
 
 })();
